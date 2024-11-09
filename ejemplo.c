79ff76c51fc626b60021d1b7a5542460600f9401
Saelemaekers <gonzarzp@gmail.com>
2024-11-09 00:37:45 -0500

    Hemos terminado de implementar la estructura que simula las funciones arrojadas en chess.c, creemos que el error podría estar relacionado con las variables locales y la forma en el que esto se almacena en el Garbage Collector.

--- ejemplo.c
+++ ejemplo.c
@@ -4,22 +4,19 @@
 
 #define SIZE 10
 
-void function(void** ptr){
-  unregisterPointer(ptr);
+void destruir(int* ptr){
+  unregisterPointer((void**)&ptr);
 }
 
+int* crear(){
+  int* ptr;
+  memoryAlloc((void**)&ptr, sizeof(int));
+  return ptr;
+}
 void display(){
-  int** ptr;
-  memoryAlloc((void**)&ptr, sizeof(int*) * SIZE);
-  for (int i=0; i<SIZE; ++i)
-    memoryAlloc((void**)&ptr[i], sizeof(int) * SIZE); 
+  int* ptr = crear();
   countMemoryEntries();
-  
-  for (int i=0; i<SIZE; ++i)
-    function((void**)&ptr[i]);
-  countMemoryEntries();
-  
-  function((void**)&ptr);
+  destruir(ptr); 
   garbageCollector();
   countMemoryEntries();
 }
