45f01d9b63fbe81351271482d87209da81a0ff87
All23tor <acc.dummyagta1@gmail.com>
2024-11-08 23:51:02 -0500

    Prueba de que se libera la memoria de un doble puntero, representando una matriz de 10x10 correctamente, esto sugiere que el error no se encuentra en la implementación del garbage collector, sino en las funciones que automatizan el proceso en caso de las piezas de ajedrez (unlinkMemory).

--- ejemplo.c
+++ ejemplo.c
@@ -2,10 +2,19 @@
 #include "figures.h"
 #include "gc.h"
 
+#define SIZE 10
+
 void display(){
   int** ptr;
-  memoryAlloc((void**)&ptr, sizeof(int*));
+  memoryAlloc((void**)&ptr, sizeof(int*) * SIZE);
+  for (int i=0; i<SIZE; ++i)
+    memoryAlloc((void**)&ptr[i], sizeof(int) * SIZE); 
   countMemoryEntries();
+  
+  for (int i=0; i<SIZE; ++i)
+    unregisterPointer((void**)&ptr[i]);
+  countMemoryEntries();
+  
   unregisterPointer((void**)&ptr);
   garbageCollector();
   countMemoryEntries();
