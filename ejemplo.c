353f45de80d91b53df51ab6333880925c56ba7a6
All23tor <acc.dummyagta1@gmail.com>
2024-11-09 23:30:21 -0500

    Imprimimos el puntero y su dirección en cada función, vemos que todo tiene el mismo contenido, pero que, al ser variables locales distintas, lógicamente sus direcciones también lo son.

--- ejemplo.c
+++ ejemplo.c
@@ -1,22 +1,34 @@
 #include "chess.h"
 #include "figures.h"
 #include "gc.h"
+#include <stdio.h>
 
 #define SIZE 10
 
-void destruir(int* ptr){
+void destruir(int* ptr){ 
+  fprintf(stderr, "destruir()\n");
+  fprintf(stderr, "Puntero: %p\n", ptr);
+  fprintf(stderr, "Dirección: %p\n", &ptr);
   unregisterPointer((void**)&ptr);
 }
 
 int* crear(){
   int* ptr;
   memoryAlloc((void**)&ptr, sizeof(int));
+  fprintf(stderr, "crear()\n");
+  fprintf(stderr, "Puntero: %p\n", ptr);
+  fprintf(stderr, "Dirección: %p\n", &ptr);
   return ptr;
 }
+
 void display(){
   int* ptr = crear();
   countMemoryEntries();
+  fprintf(stderr, "main()\n");
+  fprintf(stderr, "Puntero: %p\n", ptr);
+  fprintf(stderr, "Dirección: %p\n", &ptr);
   destruir(ptr); 
   garbageCollector();
   countMemoryEntries();
+    
 }
