b1d41f4bcaefeff7d81652a02d228dfc008ee34e
ediazva <ediazva@unsa.edu.pe>
2024-11-09 23:58:05 -0500

    Queremos intentar acceder al contenido de la lista de memoria del Garbage Collector mediante la variable memoryList, pues creemos que el error se encuentra en el contenido de esta, sin embargo no se pudo compilar. Realizaremos más experimentos para encontrar alternativas de cómo hacerlo.

--- ejemplo.c
+++ ejemplo.c
@@ -3,24 +3,6 @@
 #include "gc.h"
 #include <stdio.h>
 
-#define SIZE 10
-
-void destruir(int* ptr){ 
-  fprintf(stderr, "destruir()\n");
-  fprintf(stderr, "Puntero: %p\n", ptr);
-  fprintf(stderr, "Dirección: %p\n", &ptr);
-  unregisterPointer((void**)&ptr);
-}
-
-int* crear(){
-  int* ptr;
-  memoryAlloc((void**)&ptr, sizeof(int));
-  fprintf(stderr, "crear()\n");
-  fprintf(stderr, "Puntero: %p\n", ptr);
-  fprintf(stderr, "Dirección: %p\n", &ptr);
-  return ptr;
-}
-
 void display(){
   char** blackSquare = reverse(whiteSquare);
   fprintf(stderr, "main()\n");
@@ -28,4 +10,5 @@ void display(){
   fprintf(stderr, "Dirección: %p\n", &blackSquare);
   interpreter(blackSquare);
   garbageCollector();
+  fprintf(stderr, "memoryList = %p\n", memoryList);
 }
