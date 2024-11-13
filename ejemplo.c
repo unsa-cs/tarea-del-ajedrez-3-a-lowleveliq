c8931b0b647ade3c981c9af2b843f46413451387
All23tor <acc.dummyagta1@gmail.com>
2024-11-12 23:47:54 -0500

    LLamamos a la función reverse() múltiples veces, y vemos que funciona correctamente cada vez.

--- ejemplo.c
+++ ejemplo.c
@@ -1,10 +1,11 @@
 #include "chess.h"
 #include "figures.h"
 #include "gc.h"
-#include <stdio.h>
 
 void display(){
   char** blackSquare = reverse(whiteSquare);
+  char** blakcKnight = reverse(knight);
+  char** blakcQueen = reverse(queen);
   interpreter(blackSquare);
   garbageCollector();
 }
