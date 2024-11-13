79a41199adc95bd5a4bc50db9cd1832bae6fc5de
All23tor <acc.dummyagta1@gmail.com>
2024-11-12 23:54:40 -0500

    Revisando la documentación vimos el uso de la función registerPointerToMemory() en el código de ejemplo, vimos que esta función no estaba implementada en el gc.c pero que había otra con un propósito similar así que la renombramos para que la implemente, sin embargo al ejecutar el código de ejemplo, nos lanza un error inesperado, por lo que la implementación de una de las funciones debe estar fallando.

--- ejemplo.c
+++ ejemplo.c
@@ -1,11 +1,16 @@
 #include "chess.h"
 #include "figures.h"
 #include "gc.h"
+#include <stdio.h>
 
-void display(){
-  char** blackSquare = reverse(whiteSquare);
-  char** blakcKnight = reverse(knight);
-  char** blakcQueen = reverse(queen);
-  interpreter(blackSquare);
+void display(){ 
+  int *a;
+  memoryAlloc((void **)&a, sizeof(int));
+  int *b;
+  registerPointerToMemory((void **)&b, (void **)&a);
+  *b = 3;
+  printf("a = %d\n", *a); // debe imprimir a = 3;
+  unregisterPointer((void **)&a);
+  unregisterPointer((void **)&b);
   garbageCollector();
 }
