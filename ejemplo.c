c6f5db0274e72f77f4f4ec44b52df05f7190e89f
ediazva <ediazva@unsa.edu.pe>
2024-11-13 23:17:46 -0500

    Realizamos un ejemplo para verificar que llamar a registerPointerToMemory desde otra función funcione correctamente. Aplicamos la misma estructura que las funciones del archivo gc.c

--- ejemplo.c
+++ ejemplo.c
@@ -3,14 +3,32 @@
 #include "gc.h"
 #include <stdio.h>
 
+void registrar(int **dst, void *src) {
+  registerPointerToMemory((void **)dst, src);
+}
+
 void display(){ 
   int *a;
   memoryAlloc((void **)&a, sizeof(int));
+  *a = 45;
+  printf("a = %d\n", *a);
+
   int *b;
-  registerPointerToMemory((void **)&b, (void *)a);
+  registrar(&b, (void *)a);
   *b = 3;
-  printf("a = %d\n", *a); // debe imprimir a = 3;
+  
+  int* c;
+  registrar(&c, (void *)a);
+  *c = 6;
+
+  printf("a = %d\n", *a); // debe imprimir a = 6;
+  printf("b = %d\n", *b); // debe imprimir b = 6;
+  printf("c = %d\n", *c); // debe imprimir c = 6;
+  
+  countMemoryEntries();
   unregisterPointer((void **)&a);
   unregisterPointer((void **)&b);
+  unregisterPointer((void **)&c);
   garbageCollector();
+  countMemoryEntries();
 }
